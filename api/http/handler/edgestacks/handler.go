e4f2b33445e004656c912a90a9fd8af9aafb31a4
Chaim Lev-Ari <chiptus@gmail.com>
2020-04-28 14:35:57 +0300

    feat(edge-stacks): connect new edge stack to endpoint

--- api/http/handler/edgestacks/handler.go
+++ api/http/handler/edgestacks/handler.go
@@ -12,10 +12,14 @@ import (
 // Handler is the HTTP handler used to handle endpoint group operations.
 type Handler struct {
 	*mux.Router
-	AuthorizationService *portainer.AuthorizationService
-	EdgeStackService     portainer.EdgeStackService
-	FileService          portainer.FileService
-	GitService           portainer.GitService
+	AuthorizationService    *portainer.AuthorizationService
+	EdgeGroupService        portainer.EdgeGroupService
+	EdgeStackService        portainer.EdgeStackService
+	EndpointService         portainer.EndpointService
+	EndpointGroupService    portainer.EndpointGroupService
+	EndpointRelationService portainer.EndpointRelationService
+	FileService             portainer.FileService
+	GitService              portainer.GitService
 }
 
 // NewHandler creates a handler to manage endpoint group operations.
